f7b3848e4803cf722091ef6e597632c5affe91b2
Murat Kasimov <iokasimov.m@gmail.com>
2020-04-23 17:40:50 +0300

    Update Waterflow problem for Pandora

--- pandora/Waterflow.hs
+++ pandora/Waterflow.hs
@@ -1,7 +1,6 @@
 import "pandora" Pandora.Core
 import "pandora" Pandora.Paradigm
 import "pandora" Pandora.Pattern
-import "pandora-io" Pandora.IO
 
 import Prelude (Int, print)
 import qualified Prelude as P
@@ -11,14 +10,11 @@ getmax x = modify (x \/) *> current
 
 type Peaksearch t u a = (Traversable t, Applicative u, Pointable u, Stateful a u, Supremum a)
 
-leftmax:: Peaksearch t u a => t a -> u :. t := a
-leftmax xs = xs ->> getmax
+themax:: Peaksearch t u a => t a -> u :. t := a
+themax xs = xs ->> getmax
 
-rightmax :: Peaksearch t u a => t a -> u :. t := a
-rightmax xs = run $ xs ->> Backwards . getmax
-
-highest :: (Traversable t, Monoid a) => (State a :. t := a) -> t a
-highest = extract . run % zero
+evaluate :: (Traversable t, Monoid a) => State a (t a) -> t a
+evaluate = extract . run % zero
 
 --------------------------------------------------------------------------------
 
@@ -31,12 +27,8 @@ walls :: Stack Int
 walls = push 2 $ push 5 $ push 1 $ push 2 $ push 3 $ push 4 $ push 7 $ push 7 $ push 6 $ empty
 
 main = do
-    let (ll :*: lms) = run @(State Int) % 0 . leftmax $ walls
-    -- let rms = run % zero . rightmax $ walls
-    -- let r = (\l r x -> (l /\ r) P.- x) <$> lms <*> rms <*> walls
-    -- let r = (P.+) <$> walls <*> walls :: Stack Int
-    lms ->> print
-    print ll
-    -- print $ (0 \/ 1 :: Int)
-    -- print $ attached $ run @(State Int) (modify @Int (P.+ 1)) 0
-    -- print "typechecked"
+	let lhs = evaluate . themax $ walls
+	let rhs = run . evaluate . themax $ Reverse walls
+	let volume l x r = (l /\ r) P.- x
+	let result = volume <$> lhs <*> walls <*> rhs
+	print $ fold 0 (+) result
