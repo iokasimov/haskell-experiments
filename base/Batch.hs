20b2741d6ebca24791f6e422937f60a5f8dd1807
Murat Kasimov <iokasimov.m@gmail.com>
2020-06-05 10:13:30 +0300

    [WIP] Try to implement batching

--- base/Batch.hs
+++ base/Batch.hs
@@ -2,6 +2,8 @@ module Main where
 
 -- Source: paper "Understanding Idiomatic Traversals Backwards and Forwards"
 
+import Data.Functor
+
 data Batch e r a = Barely a | Plan e (Batch e r (r -> a))
 
 instance Functor (Batch e r) where
@@ -39,4 +41,47 @@ instance Traversable Bin where
 
 example = Bin (Bin (Tip 1) (Tip 2)) (Tip 3)
 
-main = runWith print . traverse batch $ example
+--------------------------------------------------------------------------------
+
+data Sort x y a where
+	Pure :: a -> Sort x y a
+	Sort :: x -> Sort x y (y -> a) -> Sort x y a
+
+instance Show x => Show (Sort x y a) where
+	show (Pure _) = "Pure"
+	show (Sort o f) = "Sort (" ++ show o ++ ") (" ++ show f ++ ")"
+
+instance Functor (Sort x y) where
+	fmap f (Pure x) = Pure $ f x
+	fmap f (Sort a b) = Sort a $ (f .) <$> b
+
+instance Ord x => Applicative (Sort x y) where
+	pure = Pure
+	Pure f <*> a = f <$> a
+	f <*> Pure a = ($ a) <$> f
+	Sort a f <*> Sort b g = if a < b
+		then Sort a $ flip <$> f <*> Sort b g
+		else Sort b $ flip <$> f <*> Sort a g
+	 	-- else Sort b $ flip <$> f <*> Sort a g
+
+-- f :: Sort x y (y -> a -> b)
+-- g :: Sort x y (y -> a)
+
+liftSort :: x -> Sort x y y
+liftSort a = Sort a $ Pure id
+
+runSort :: (x -> y) -> Sort x y a -> a
+runSort _ (Pure a) = a
+runSort f (Sort a g) = runSort f g (f a)
+
+sortTraversable :: (Ord a, Traversable t) => t a -> t a
+sortTraversable = runSort id . traverse liftSort
+
+--------------------------------------------------------------------------------
+
+-- main = runWith print . traverse batch $ example
+
+-- main = print $ sortTraversable [1,3,4,2,6,9,13,8]
+main = do
+	print $ sortTraversable [1,3,4,2,6,9,13,12,8]
+	print $ sortTraversable [10,9,8,7,6,5,4,3,2,1]
