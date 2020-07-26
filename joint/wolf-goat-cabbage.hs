8e1994f2a0e372639f251f9670fa895a3cc423c6
Murat Kasimov <iokasimov.m@gmail.com>
2020-07-26 06:28:50 +0300

    Implement wolf, goat and cabbage problem with Joint

--- joint/wolf-goat-cabbage.hs
+++ joint/wolf-goat-cabbage.hs
@@ -1,9 +1,9 @@
-
-import "base" Control.Monad (guard)
+import "base" Data.Bool (bool)
+import "base" Data.Functor (($>))
 import "base" Data.List (delete)
-import "lens" Control.Lens (Lens', view, _1, _2, (&), (.~), (%~))
-import "joint" Control.Joint (Stateful, Failable, State, lift
-	, current, modify, failure, run, type (:>), type (:=))
+import "lens" Control.Lens (Lens', view, _1, _2, (%~))
+import "joint" Control.Joint (Liftable, Stateful, State, lift
+	, current, modify, run, type (:>), type (:=))
 
 data Character = Wolf | Goat | Cabbage
 	deriving (Eq, Show)
@@ -32,7 +32,8 @@ alter :: Bank -> Bank
 alter Initial = Far
 alter Far = Initial
 
-data Move = Move Bank Character
+data Move = Move Bank Character | Vain Bank
+	deriving Show
 
 type River = ([Character],[Character])
 
@@ -44,21 +45,38 @@ source, target :: Bank -> Lens' River [Character]
 source = coast . alter
 target = coast
 
-transport :: Stateful River t => Bank -> Character -> t ()
-transport bank candidate = modify @River $ arrive . depart where
+transport :: (Applicative t, Stateful River t) => Bank -> Character -> t Move
+transport bank candidate = modify @River (arrive . depart) $> Move bank candidate where
 
+	depart, arrive :: River -> River
 	depart = source bank %~ delete candidate
 	arrive = target bank %~ (candidate :)
 
-step :: Bank -> State River :> [] := ()
-step bank = do
-	from <- view (source bank) <$> current
-	candidate <- lift from
-	to <- view (target bank) <$> current
-	lift @[] . guard $ all (coexist candidate) to
-	transport bank candidate
+finished :: (Functor t, Stateful River t) => t (Maybe Character)
+finished = last_one . view (coast Initial) <$> current where
+
+	last_one :: [Character] -> Maybe Character
+	last_one (one : []) = Just $ one
+	last_one _ = Nothing
+
+attempt :: (Monad t, Stateful River t, Liftable [] t) => Bank -> t Move
+attempt bank = compatibility <$> next <*> destination
+	>>= bool (pure $ Vain bank) (next >>= transport bank) where
+
+	next :: (Monad t, Stateful River t, Liftable [] t) => t Character
+	next = view (source bank) <$> current >>= lift
+
+	compatibility :: Character -> [Character] -> Bool
+	compatibility candidate others = all (coexist candidate) others
+
+	destination :: (Functor t, Stateful River t) => t [Character]
+	destination = view (target bank) <$> current
+
+step :: Bank -> State River :> [] := Move
+step Far = finished >>= maybe (attempt Far) (transport Far)
+step Initial = attempt Initial
 
 start :: River
 start = ([Wolf, Goat, Cabbage], [])
 
-main = print $ run (step Far) start
+main = traverse print $ run (traverse step $ take 5 route) start
