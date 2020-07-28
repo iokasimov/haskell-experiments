a9bc4424ef90d69b925c88f6f921f6156a10fe01
Murat Kasimov <iokasimov.m@gmail.com>
2020-07-28 07:11:43 +0100

    Fix Wolf, Goat and Cabbage problem solution

--- joint/wolf-goat-cabbage.hs
+++ joint/wolf-goat-cabbage.hs
@@ -1,82 +1,66 @@
 import "base" Data.Bool (bool)
 import "base" Data.Functor (($>))
 import "base" Data.List (delete)
-import "lens" Control.Lens (Lens', view, _1, _2, (%~))
+import "base" Control.Applicative ((<|>))
+import "lens" Control.Lens (Lens', view, _1, _2, _3, (.~), (%~))
 import "joint" Control.Joint (Liftable, Stateful, State, lift
-	, current, modify, run, type (:>), type (:=))
+	, current, modify, nothing, run, type (:>), type (:=), (<$$>), (<**>))
 
 data Character = Wolf | Goat | Cabbage
 	deriving (Eq, Show)
 
 instance Ord Character where
-	compare Wolf Wolf = EQ
 	compare Wolf Goat = GT
-	compare Wolf Cabbage = EQ
 	compare Goat Wolf = LT
-	compare Goat Goat = EQ
 	compare Goat Cabbage = GT
-	compare Cabbage Wolf = EQ
 	compare Cabbage Goat = LT
-	compare Cabbage Cabbage = EQ
+	compare _ _ = EQ
 
 coexist :: Ord a => a -> a -> Bool
 coexist x y = compare x y == EQ
 
-data Bank = Initial | Far
-	deriving Show
+data Direction = Back | Forward
 
-route :: [Bank]
-route = iterate alter Far
+route :: [Direction]
+route = iterate alter Forward
 
-alter :: Bank -> Bank
-alter Initial = Far
-alter Far = Initial
+alter :: Direction -> Direction
+alter Back = Forward
+alter Forward = Back
 
-data Move = Move Bank Character | Vain Bank
-	deriving Show
+type River = ([Character], [Character])
 
-type River = ([Character],[Character])
+type Iterable = Liftable []
 
-coast :: Bank -> Lens' River [Character]
-coast Initial = _1
-coast Far = _2
+coast :: Direction -> Lens' River [Character]
+coast Back = _1
+coast Forward = _2
 
-source, target :: Bank -> Lens' River [Character]
+source, target :: Direction -> Lens' River [Character]
 source = coast . alter
 target = coast
 
-transport :: (Applicative t, Stateful River t) => Bank -> Character -> t Move
-transport bank candidate = modify @River (arrive . depart) $> Move bank candidate where
+step :: Direction -> State River :> [] := Maybe Character
+step direction = bank >>= next >>= transport where
 
-	depart, arrive :: River -> River
-	depart = source bank %~ delete candidate
-	arrive = target bank %~ (candidate :)
+	bank :: (Functor t, Stateful River t) => t [Character]
+	bank = view (source direction) <$> current
 
-finished :: (Functor t, Stateful River t) => t (Maybe Character)
-finished = last_one . view (coast Initial) <$> current where
+	next :: Iterable t => [Character] -> t (Maybe Character)
+	next coast = lift $ filter valid $ Nothing : (Just <$> coast) where
 
-	last_one :: [Character] -> Maybe Character
-	last_one (one : []) = Just $ one
-	last_one _ = Nothing
+		valid :: Maybe Character -> Bool
+		valid Nothing = and $ coexist <$> coast <*> coast
+		valid (Just x) = and $ coexist <$> delete x coast <*> delete x coast
 
-attempt :: (Monad t, Stateful River t, Liftable [] t) => Bank -> t Move
-attempt bank = compatibility <$> next <*> destination
-	>>= bool (pure $ Vain bank) (next >>= transport bank) where
+	transport Nothing = pure Nothing
+	transport (Just character) = modify @River (leave . land) $> Just character where
 
-	next :: (Monad t, Stateful River t, Liftable [] t) => t Character
-	next = view (source bank) <$> current >>= lift
-
-	compatibility :: Character -> [Character] -> Bool
-	compatibility candidate others = all (coexist candidate) others
-
-	destination :: (Functor t, Stateful River t) => t [Character]
-	destination = view (target bank) <$> current
-
-step :: Bank -> State River :> [] := Move
-step Far = finished >>= maybe (attempt Far) (transport Far)
-step Initial = attempt Initial
+		leave, land :: River -> River
+		leave = source direction %~ delete character
+		land = target direction %~ (character :)
 
 start :: River
-start = ([Wolf, Goat, Cabbage], [])
+start = ([Goat, Wolf, Cabbage], [])
 
-main = traverse print $ run (traverse step $ take 5 route) start
+main = traverse print $ filter ((==) [] . fst . fst) $ run (traverse step $ take 7 route) start
