b4199cb63cce3deba9b517144a23d21333877601
Murat Kasimov <iokasimov.m@gmail.com>
2020-07-29 08:51:03 +0100

    Refactor a bit Wolf, Goat and Cabbage solution with Joint

--- joint/wolf-goat-cabbage.hs
+++ joint/wolf-goat-cabbage.hs
@@ -1,13 +1,15 @@
-import "base" Data.Bool (bool)
 import "base" Data.Functor (($>))
 import "base" Data.List (delete)
-import "base" Control.Applicative ((<|>))
-import "lens" Control.Lens (Lens', view, _1, _2, _3, (.~), (%~))
+import "lens" Control.Lens (Lens', view, _1, _2, (%~))
 import "joint" Control.Joint (Liftable, Stateful, State, lift
-	, current, modify, nothing, run, type (:>), type (:=), (<$$>), (<**>))
+	, current, modify, run, type (:>), type (:=))
 
-data Character = Wolf | Goat | Cabbage
-	deriving (Eq, Show)
+data Character = Wolf | Goat | Cabbage deriving Eq
+
+instance Show Character where
+	show Wolf = "🐺"
+	show Goat = "🐐"
+	show Cabbage = "🥬"
 
 instance Ord Character where
 	compare Wolf Goat = GT
@@ -21,24 +23,15 @@ coexist x y = compare x y == EQ
 
 data Direction = Back | Forward
 
-route :: [Direction]
-route = iterate alter Forward
-
-alter :: Direction -> Direction
-alter Back = Forward
-alter Forward = Back
-
 type River = ([Character], [Character])
 
 type Iterable = Liftable []
 
-coast :: Direction -> Lens' River [Character]
-coast Back = _1
-coast Forward = _2
-
 source, target :: Direction -> Lens' River [Character]
-source = coast . alter
-target = coast
+source Back = _2
+source Forward = _1
+target Back = _1
+target Forward = _2
 
 step :: Direction -> State River :> [] := Maybe Character
 step direction = bank >>= next >>= transport where
@@ -47,11 +40,11 @@ step direction = bank >>= next >>= transport where
 	bank = view (source direction) <$> current
 
 	next :: Iterable t => [Character] -> t (Maybe Character)
-	next coast = lift $ filter valid $ Nothing : (Just <$> coast) where
+	next characters = lift $ filter valid $ Nothing : (Just <$> characters) where
 
 		valid :: Maybe Character -> Bool
-		valid Nothing = and $ coexist <$> coast <*> coast
-		valid (Just x) = and $ coexist <$> delete x coast <*> delete x coast
+		valid Nothing = and $ coexist <$> characters <*> characters
+		valid (Just c) = and $ coexist <$> delete c characters <*> delete c characters
 
 	transport Nothing = pure Nothing
 	transport (Just character) = modify @River (leave . land) $> Just character where
@@ -63,4 +56,12 @@ step direction = bank >>= next >>= transport where
 start :: River
 start = ([Goat, Wolf, Cabbage], [])
 
-main = traverse print $ filter ((==) [] . fst . fst) $ run (traverse step $ take 7 route) start
+route :: [Direction]
+route = iterate alter Forward where
+
+	alter :: Direction -> Direction
+	alter Back = Forward
+	alter Forward = Back
+
+main = traverse print $ filter ((==) [] . fst . fst)
+	$ run (traverse step $ take 7 route) start
