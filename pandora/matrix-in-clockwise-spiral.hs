33ff64ab7c2a79c7a26a4bfb6fe8cfc4c70b9e62
Murat Kasimov <iokasimov.m@gmail.com>
2020-08-03 09:31:46 +0300

    Try to construct vector and matrices with product datatype

---
+++ pandora/matrix-in-clockwise-spiral.hs
@@ -0,0 +1,38 @@
+import "pandora" Pandora.Core
+import "pandora" Pandora.Paradigm
+import "pandora" Pandora.Pattern
+import "pandora-io" Pandora.IO
+
+import Prelude (Int, print)
+import qualified Prelude as P
+
+type Vector v a = Monotonic v a
+
+vector_total_sum :: Vector v Int => v -> Int
+vector_total_sum = iterate (P.+) (0 :: Int)
+
+type Matrix m v a = (Monotonic m v, Vector v a)
+
+--------------------------------------------------------------------------------
+
+-- [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]]
+-- ==> 1 2 3 4 5 10 15 20 19 18 17 16 11 6 7 8 9 14 13 12
+
+type M54 a = V5 a :*: V5 a :*: V5 a :*: V5 a
+
+matrix_example :: M54 Int
+matrix_example = (1 :*: 2 :*: 3 :*: 4 :*: 5)
+    :*: (6 :*: 7 :*: 8 :*: 9 :*: 10)
+    :*: (11 :*: 12 :*: 13 :*: 14 :*: 15)
+    :*: (16 :*: 17 :*: 18 :*: 19 :*: 20)
+
+matrix_diagonal :: Matrix m v a => m a -> v a
+
+type V5 a = a :*: a :*: a :*: a :*: a
+
+vector_example :: V5 Int
+vector_example = 1 :*: 2 :*: 3 :*: 4 :*: 5
+
+main = do
+    print $ vector_total_sum vector_example
+    print $ iterate (\x r -> r P.+ vector_total_sum @(V5 Int) x) (0 :: Int) matrix_example
