1653cea219dcf492f63f42712ee7d030c6c81154
Murat Kasimov <iokasimov.m@gmail.com>
2020-09-06 10:27:03 +0300

    Try some code from paper 'Cheap (but functional) threads'

---
+++ base/resumption.hs
@@ -0,0 +1,22 @@
+import "transformers" Control.Monad.Trans.State
+
+-- Source: Cheap (but functional) threads
+
+data Resumption a = Done a | Pause (State Int (Resumption a))
+
+instance Functor Resumption where
+	fmap f (Done x) = Done $ f x
+	fmap f (Pause r) = Pause $ f <$> r
+
+instance Applicative Resumption where
+	Done f <*> Done x = Done $ f x
+	Done f <*> Pause r = Done f <*> r
+	Pause f <*> Done x = f <*> Done x
+	Pause f <*> Pause r = f <*> r
+	pure = Done
+
+instance Monad Resumption where
+	Done x >>= f = f x
+	Pause r >>= f = Pause $ r >>= f
+
+main = print "typechecked"
