56c4e7f0eaebd04d312f3bf6cb9620c514f95492
Murat Kasimov <iokasimov.m@gmail.com>
2020-11-15 07:07:20 +0300

    Update Pandora version

--- pandora/ca-rule.hs
+++ pandora/ca-rule.hs
@@ -6,18 +6,15 @@ import Prelude (Char, Show, print)
 
 type Status = Boolean
 
-type Field = Zipper (Nonempty Stack) Status
+type Field = Zipper Stream Status
 
 type Neighbours = Status :*: Status :*: Status
 
 start :: Field
 start = Tap True . TU $ desert :^: desert where
 
-	desert :: Nonempty Stack Status
-	desert = insert False $ insert False
-		$ insert False $ insert False $ insert False
-		$ insert False $ insert False $ insert False
-		$ insert False $ point False
+	desert :: Stream Status
+	desert = repeat False
 
 rule50 :: Neighbours -> Status
 rule50 (True :*: True :*: True) = False
@@ -29,21 +26,30 @@ rule50 (False :*: True :*: False) = False
 rule50 (False :*: False :*: True) = True
 rule50 (False :*: False :*: False) = False
 
-neighbourhood :: Field -> Neighbours
-neighbourhood z = leftward :*: extract z :*: rightward where
-
-	leftward, rightward :: Status
-	leftward = vacant $ extract <$> backward' z
-	rightward = vacant $ extract <$> forward' z
-
-	vacant :: Maybe Status -> Status
-	vacant = maybe False identity
+-- neighbourhood :: Field -> Neighbours
+-- neighbourhood z = leftward :*: extract z :*: rightward where
+--
+-- 	leftward, rightward :: Status
+-- 	leftward = vacant $ extract <$> backward' z
+-- 	rightward = vacant $ extract <$> forward' z
+--
+-- 	vacant :: Maybe Status -> Status
+-- 	vacant = maybe False identity
 
 deriving instance Show Boolean
 deriving instance (Show a, Show b) => Show (a :*: b)
 deriving instance Show a => Show (Maybe a)
+deriving instance (Show e, Show a) => Show (Conclusion e a)
 
-main = print . extract $ start
+instance Monotonic (Construction Identity a) a where
+	bypass f r ~(Construct x (Identity xs)) = f x $ bypass f r xs
+
+main = do
+    print . extract $ repeat False
+    -- print . find (equate False) $ True :*: False :*: True
+    -- print . find (equate False) $ insert True $ insert True $ insert False $ empty @Stack
+
+-- main = print . extract $ start
 	-- print . extract $ start =>> rule50 . neighbourhood
 	-- print . extract $ start =>> rule50 . neighbourhood =>> rule50 . neighbourhood
 	-- print . extract $ start =>> rule50 . neighbourhood =>> rule50 . neighbourhood =>> rule50 . neighbourhood
--- pandora/matrix-in-clockwise-spiral.hs
+++ pandora/matrix-in-clockwise-spiral.hs
@@ -1,7 +1,6 @@
 import "pandora" Pandora.Core
 import "pandora" Pandora.Paradigm
 import "pandora" Pandora.Pattern
--- import "pandora-io" Pandora.IO
 
 import Prelude (Int, Show, print)
 import qualified Prelude as P
@@ -9,7 +8,7 @@ import qualified Prelude as P
 type Vector v a = Monotonic v a
 
 vector_total_sum :: Vector v Int => v -> Int
-vector_total_sum = iterate (P.+) (0 :: Int)
+vector_total_sum = bypass (P.+) (0 :: Int)
 
 type Matrix m v a = (Monotonic m v, Vector v a)
 
@@ -35,10 +34,10 @@ deriving instance (Show a, Show b) => Show (a :*: b)
 instance Semigroup Int where (+) = (P.+)
 
 last :: V5 Int -> Int
-last = iterate (\x r -> x) P.undefined
+last = bypass (\x r -> x) P.undefined
 
 main = do
 	print $ vector_total_sum vector_example
-	print $ iterate (\x r -> r P.+ vector_total_sum @(V5 Int) x) (0 :: Int) matrix_example
+	print $ bypass (\x r -> r P.+ vector_total_sum @(V5 Int) x) (0 :: Int) matrix_example
 	print ((1 :*: 2 :*: 3 :*: 4 :*: 5) + (6 :*: 7 :*: 8 :*: 9 :*: 10) :: V5 Int)
 	print $ last vector_example
