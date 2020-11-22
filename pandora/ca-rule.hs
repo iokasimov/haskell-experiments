8a8d51db86ef132d1646f36fcfd261bc591bb9ef
Murat Kasimov <iokasimov.m@gmail.com>
2020-11-22 10:11:23 +0300

    [WIP] Comonoadic cellular automata

--- pandora/ca-rule.hs
+++ pandora/ca-rule.hs
@@ -2,11 +2,19 @@ import "pandora" Pandora.Core
 import "pandora" Pandora.Paradigm
 import "pandora" Pandora.Pattern
 
-import Prelude (Char, Show, print)
+import Prelude (Char, Int, Show, print, (<>))
 
 type Status = Boolean
 
 type Field = Zipper Stream Status
+
+instance {-# OVERLAPS #-} Extendable (Tap (Delta <:.> Stream)) where
+	zs@(Tap x (TU (bs :^: fs))) =>> f = f <$> zs_duplicate zs
+
+zs_duplicate :: Zipper Stream a -> Zipper Stream (Zipper Stream a)
+zs_duplicate z = Tap z . TU $
+	(extract . deconstruct $ iterate (point . rotate @Left) z)
+		:^: (extract . deconstruct $ iterate (point . rotate @Right) z)
 
 type Neighbours = Status :*: Status :*: Status
 
@@ -26,15 +34,8 @@ rule50 (False :*: True :*: False) = False
 rule50 (False :*: False :*: True) = True
 rule50 (False :*: False :*: False) = False
 
--- neighbourhood :: Field -> Neighbours
--- neighbourhood z = leftward :*: extract z :*: rightward where
---
--- 	leftward, rightward :: Status
--- 	leftward = vacant $ extract <$> backward' z
--- 	rightward = vacant $ extract <$> forward' z
---
--- 	vacant :: Maybe Status -> Status
--- 	vacant = maybe False identity
+neighbourhood :: Field -> Neighbours
+neighbourhood z = extract (sub @Left ^. z) :*: extract z :*: extract (sub @Right ^. z)
 
 deriving instance Show Boolean
 deriving instance (Show a, Show b) => Show (a :*: b)
@@ -45,12 +46,8 @@ instance Monotonic (Construction Identity a) a where
 	bypass f r ~(Construct x (Identity xs)) = f x $ bypass f r xs
 
 main = do
-    print . extract $ repeat False
-    -- print . find (equate False) $ True :*: False :*: True
-    -- print . find (equate False) $ insert True $ insert True $ insert False $ empty @Stack
-
--- main = print . extract $ start
-	-- print . extract $ start =>> rule50 . neighbourhood
-	-- print . extract $ start =>> rule50 . neighbourhood =>> rule50 . neighbourhood
-	-- print . extract $ start =>> rule50 . neighbourhood =>> rule50 . neighbourhood =>> rule50 . neighbourhood
-	-- print . extract $ start =>> rule50 . neighbourhood =>> rule50 . neighbourhood =>> rule50 . neighbourhood =>> rule50 . neighbourhood
+	print . neighbourhood $ start
+	print . neighbourhood $ start =>> rule50 . neighbourhood
+	print . neighbourhood $ start =>> rule50 . neighbourhood =>> rule50 . neighbourhood
+	print . neighbourhood $ start =>> rule50 . neighbourhood =>> rule50 . neighbourhood =>> rule50 . neighbourhood
+	print . neighbourhood $ start =>> rule50 . neighbourhood =>> rule50 . neighbourhood =>> rule50 . neighbourhood =>> rule50 . neighbourhood
