856b9fa51d6dd7f505274e2d302f5cb82eb026eb
Murat Kasimov <iokasimov.m@gmail.com>
2020-11-23 10:53:55 +0300

    Refactor Zipper for Binary tree

--- base/binary-tree-zipper.hs
+++ base/binary-tree-zipper.hs
@@ -6,24 +6,27 @@ data Tree a = Branch (Tree a) (Tree a) | Leaf a deriving Show
 
 type Loc a = (Tree a, Context a)
 
-data Context a = Top | L (Context a) (Tree a) | R (Context a) (Tree a) deriving Show
+data Context a = Top
+    | L (Tree a) (Context a)
+    | R (Tree a) (Context a)
+    deriving Show
 
 left :: Loc a -> Loc a
-left (Branch l r, c) = (l, L c r)
+left (Branch l r, c) = (l, L r c)
 
 right :: Loc a -> Loc a
-right (Branch l r, c) = (r, R c l)
+right (Branch l r, c) = (r, R l c)
 
 top :: Tree a -> Loc a
 top t = (t, Top)
 
 up :: Loc a -> Loc a
-up (t, L c r) = (Branch t r, c)
-up (t, R c l) = (Branch l t, c)
+up (t, L r c) = (Branch t r, c)
+up (t, R l c) = (Branch l t, c)
 
 upmost :: Loc a -> Loc a
 upmost l@(t, Top) = l
-upmost l = upmost (up l)
+upmost l = upmost $ up l
 
 modify :: Loc a -> (Tree a -> Tree a) -> Loc a
 modify (t, c) f = (f t, c)
@@ -36,7 +39,7 @@ example_tree = Branch
     (Branch (Leaf 3) (Leaf 4))
 
 example_loc :: Loc Int
-example_loc = (Leaf 2, R (L Top (Branch (Leaf 3) (Leaf 4))) (Leaf 1))
+example_loc = (Leaf 2, R (Leaf 1) $ L (Branch (Leaf 3) (Leaf 4)) Top)
 
 main = do
     -- print example_tree
