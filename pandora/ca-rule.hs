4c7b430890fe53fbb89b8abbc06d2d659010e181
Murat Kasimov <iokasimov.m@gmail.com>
2020-11-30 07:36:34 +0300

    Update Pandora version

--- pandora/ca-rule.hs
+++ pandora/ca-rule.hs
@@ -2,28 +2,19 @@ import "pandora" Pandora.Core
 import "pandora" Pandora.Paradigm
 import "pandora" Pandora.Pattern
 
-import Prelude (Char, Int, Show (show), print, (<>))
+import GHC.Int (Int, eqInt)
+import Prelude (Char, Show (show), print, reverse, (-), (<>))
 import Control.Concurrent (threadDelay)
 
 type Status = Boolean
 
 type Field = Zipper Stream Status
 
-instance {-# OVERLAPS #-} Extendable (Tap (Delta <:.> Stream)) where
-	zs@(Tap x (TU (bs :^: fs))) =>> f = f <$> zs_duplicate zs
-
-zs_duplicate :: Zipper Stream a -> Zipper Stream (Zipper Stream a)
-zs_duplicate z = Tap z . TU $
-	(extract . deconstruct $ iterate (point . rotate @Left) z)
-		:^: (extract . deconstruct $ iterate (point . rotate @Right) z)
-
 type Neighbours = Status :*: Status :*: Status
 
 start :: Field
-start = Tap True . TU $ desert :^: desert where
-
-	desert :: Stream Status
-	desert = repeat False
+start = let desert = repeat False
+	in Tap True . TU $ desert :^: desert
 
 rule50 :: Neighbours -> Status
 rule50 (True :*: True :*: True) = False
@@ -38,27 +29,29 @@ rule50 (False :*: False :*: False) = False
 neighbourhood :: Field -> Neighbours
 neighbourhood z = extract (sub @Left ^. z) :*: extract z :*: extract (sub @Right ^. z)
 
-five_around :: Field -> [Status]
-five_around (Tap x (TU (bs :^: fs))) = last5 bs <> [x] <> first5 fs where
+display :: Natural -> Field -> [Status]
+display n (Tap x (TU (bs :^: fs))) = take_n n bs [] <> [x] <> reverse (take_n n fs []) where
 
-	first5, last5 :: Stream ~> []
-	first5 (Construct x (Identity (Construct x' (Identity (Construct x'' (Identity (Construct x''' (Identity (Construct x'''' _))))))))) = [x, x', x'', x''', x'''']
-	last5 (Construct x (Identity (Construct x' (Identity (Construct x'' (Identity (Construct x''' (Identity (Construct x'''' _))))))))) = [x'''', x''', x'', x', x]
+	take_n :: Natural -> Stream a -> [a] -> [a]
+	take_n (Natural n) (Construct x (Identity next)) r = take_n n next $ x : r
+	take_n Zero _ r = r
 
 instance Show Boolean where
 	show True = "*"
 	show False = " "
 
-deriving instance (Show a, Show b) => Show (a :*: b)
-deriving instance Show a => Show (Maybe a)
-deriving instance (Show e, Show a) => Show (Conclusion e a)
+instance Setoid Int where
+	x == y = if eqInt x y then True else False
+
+natural :: Int -> Natural
+natural n = n == 0 ? Zero $ Natural . natural $ n - 1
 
 instance Monotonic (Construction Identity a) a where
 	bypass f r ~(Construct x (Identity xs)) = f x $ bypass f r xs
 
 lifecycle act being = do
 	threadDelay 1000000
-	print $ five_around being
+	print $ display (natural 10) being
 	lifecycle act $ being =>> act
 
 main = lifecycle (rule50 . neighbourhood) start
