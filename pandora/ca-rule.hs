048d55f9ebf66d6e71643a6b7097fc62258eb03d
Murat Kasimov <iokasimov.m@gmail.com>
2020-12-06 10:45:36 +0300

    Apply rule for Sierpinsky triangle, work on 2D zipper

--- pandora/ca-rule.hs
+++ pandora/ca-rule.hs
@@ -16,15 +16,26 @@ start :: Field
 start = let desert = repeat False
 	in Tap True . TU $ desert :^: desert
 
-rule50 :: Neighbours -> Status
-rule50 (True :*: True :*: True) = False
-rule50 (True :*: True :*: False) = False
-rule50 (True :*: False :*: True) = True
-rule50 (True :*: False :*: False) = True
-rule50 (False :*: True :*: True) = False
-rule50 (False :*: True :*: False) = False
-rule50 (False :*: False :*: True) = True
-rule50 (False :*: False :*: False) = False
+-- rule50 :: Neighbours -> Status
+-- rule50 (True :*: True :*: True) = False
+-- rule50 (True :*: True :*: False) = False
+-- rule50 (True :*: False :*: True) = True
+-- rule50 (True :*: False :*: False) = True
+-- rule50 (False :*: True :*: True) = False
+-- rule50 (False :*: True :*: False) = False
+-- rule50 (False :*: False :*: True) = True
+-- rule50 (False :*: False :*: False) = False
+
+rule90 :: Neighbours -> Status
+rule90 (True :*: True :*: True) = False
+rule90 (True :*: True :*: False) = True
+rule90 (True :*: False :*: True) = False
+rule90 (True :*: False :*: False) = True
+rule90 (False :*: True :*: True) = True
+rule90 (False :*: True :*: False) = False
+rule90 (False :*: False :*: True) = True
+rule90 (False :*: False :*: False) = False
+
 
 neighbourhood :: Field -> Neighbours
 neighbourhood z = extract (sub @Left ^. z) :*: extract z :*: extract (sub @Right ^. z)
@@ -75,11 +86,22 @@ vd :: (Zipper Stream :. Zipper Stream := a) -> (Zipper Stream :. Zipper Stream :
 vd z = let move rtt = extract . deconstruct . iterate (point . rtt) $ z
 	in Tap z . TU $ move (rotate @Left) :^: move (rotate @Right)
 
+instance Substructure Down (Tap (Delta <:.> Stream) <:.> Tap (Delta <:.> Stream)) where
+	type Substructural Down (Tap (Delta <:.> Stream) <:.> Tap (Delta <:.> Stream)) a = Stream :. Zipper Stream := a
+	substructure (run . extract -> Tap focused (TU (down :^: up))) = Store $ down :*: Tag . TU . Tap focused . TU . (:^: up)
+
+instance Substructure Up (Tap (Delta <:.> Stream) <:.> Tap (Delta <:.> Stream)) where
+	type Substructural Up (Tap (Delta <:.> Stream) <:.> Tap (Delta <:.> Stream)) a = Stream :. Zipper Stream := a
+	substructure (run . extract -> Tap focused (TU (down :^: up))) = Store $ up :*: Tag . TU . Tap focused . TU . (down :^:)
+
+-- (left :*: right) :*: (up :*: down) :*: major diagonal :*: minor diagonal
+type Around = (Status :*: Status) :*: (Status :*: Status) :*: (Status :*: Status) :*: (Status :*: Status)
+
 --------------------------------------------------------------------------------
 
 lifecycle act being = do
 	threadDelay 1000000
-	print $ display (natural 21) being
+	print $ display (natural 25) being
 	lifecycle act $ being =>> act
 
-main = lifecycle (rule50 . neighbourhood) start
+main = lifecycle (rule90 . neighbourhood) start
