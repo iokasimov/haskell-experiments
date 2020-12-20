f874c78d313c103080a78b3b82ce13263c5d4e49
Murat Kasimov <iokasimov.m@gmail.com>
2020-12-20 11:23:24 +0300

    [WIP] Stock prices with transaction fee

--- pandora/stock-prices.hs
+++ pandora/stock-prices.hs
@@ -5,9 +5,21 @@ import "pandora" Pandora.Pattern
 import Prelude (Int, Show, print)
 import qualified Prelude as P
 
-type Prices = Nonempty Stack Int
+data Action a = Buy a | Sell a
 
-prices :: Prices
-prices = insert 10 $ insert 7 $ insert 5 $ insert 8 $ insert 11 $ Construct 9 Nothing
+-- order :: Stream := Operation ()
+-- order = point . alter .-+ Buy () where
+--
+-- 	alter :: Operation ~> Operation
+-- 	alter (Buy x) = Sell x
+-- 	alter (Sell x) = Buy x
+
+--------------------------------------------------------------------------------
+
+-- example :: Nonempty Stack Int
+-- example = insert 1 $ insert 3 $ insert 2 $ insert 8 $ insert 4 $ Construct 10 Nothing
+
+example :: Stack Int
+example = insert 1 $ insert 3 $ insert 2 $ insert 8 $ insert 4 $ insert 10 $ empty
 
 main = print "Work in progress..."
