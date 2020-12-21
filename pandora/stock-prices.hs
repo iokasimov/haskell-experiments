3aa1c998736986e86674aa37bf8dfea43b941010
Murat Kasimov <iokasimov.m@gmail.com>
2020-12-21 16:01:45 +0300

    [WIP] Stock prices with Pandora

--- pandora/stock-prices.hs
+++ pandora/stock-prices.hs
@@ -1,25 +1,45 @@
 import "pandora" Pandora.Core
 import "pandora" Pandora.Paradigm
 import "pandora" Pandora.Pattern
+import "pandora-io" Pandora.IO
 
 import Prelude (Int, Show, print)
-import qualified Prelude as P
 
-data Action a = Buy a | Sell a
+type Prices = Nonempty Stack Int
 
--- order :: Stream := Operation ()
--- order = point . alter .-+ Buy () where
---
--- 	alter :: Operation ~> Operation
--- 	alter (Buy x) = Sell x
--- 	alter (Sell x) = Buy x
+step :: Prices -> Maybe :. Nonempty Stack := Int :*: Int
+step prices = (:*:) buy <$$> sales where
+
+	buy :: Int
+	buy = extract prices
+
+	sales :: Maybe :. Nonempty Stack := Int
+	sales = deconstruct prices
 
 --------------------------------------------------------------------------------
 
--- example :: Nonempty Stack Int
--- example = insert 1 $ insert 3 $ insert 2 $ insert 8 $ insert 4 $ Construct 10 Nothing
+example :: Prices
+example = insert 10 $ insert 7 $ insert 5 $ insert 8 $ insert 11 $ point 9
+-- example = insert 1 $ insert 3 $ insert 2 $ insert 8 $ insert 4 $ point 10
 
-example :: Stack Int
-example = insert 1 $ insert 3 $ insert 2 $ insert 8 $ insert 4 $ insert 10 $ empty
+-- x = run $ Comprehension example >>= Comprehension . insert 0 . insert % empty
+-- x = (:*:) <$> Comprehension example <*> Comprehension example
 
-main = print "Work in progress..."
+listify :: [a] -> Nonempty Stack a -> [a]
+listify r (Construct x (Just next)) = listify (x : r) next
+listify r (Construct x Nothing) = x : r
+
+instance Covariant [] where
+	f <$> [] = []
+	f <$> (x : xs) = (f x) : (f <$> xs)
+
+instance Traversable [] where
+	[] ->> _ = point []
+	(x : xs) ->> f = (:) <$> f x <*> xs ->> f
+
+deriving instance (Show a, Show b) => Show (a :*: b)
+deriving instance Show a => Show (Maybe a)
+
+main = do
+	print "typechecked"
+	(listify [] <$$> listify [] (example =>> step)) ->> print
