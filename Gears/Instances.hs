33aed0d086e69995486d497a34294d5f0313fde4
Murat Kasimov <iokasimov.m@gmail.com>
2020-12-26 09:16:48 +0300

    Define Ringoid and Group instances for Int

--- Gears/Instances.hs
+++ Gears/Instances.hs
@@ -4,8 +4,8 @@ import "pandora" Pandora.Core
 import "pandora" Pandora.Paradigm
 import "pandora" Pandora.Pattern
 
-import qualified GHC.Int as Base (eqInt)
-import qualified Prelude as Base (Int, Semigroup ((<>)), Show (show), min, max, (+), (-))
+import qualified GHC.Int as Base -- (eqInt)
+import qualified Prelude as Base -- (Int, Semigroup ((<>)), Show (show), min, max, (+), (-), (*))
 
 instance Covariant [] where
 	f <$> [] = []
@@ -27,10 +27,26 @@ instance Base.Show Natural where
 	show Zero = "0"
 	show (Natural n) = "1" Base.<> Base.show n
 
-instance Semigroup Base.Int where (+) = (Base.+)
-instance Monoid Base.Int where zero = 0
-instance Infimum Base.Int where (/\) = Base.min
-instance Supremum Base.Int where (\/) = Base.max
+instance Semigroup Base.Int where
+	(+) = (Base.+)
+
+instance Monoid Base.Int where
+	zero = 0
+
+instance Ringoid Base.Int where
+	(*) = (Base.*)
+
+instance Quasiring Base.Int where
+	one = 1
+
+instance Group Base.Int where
+	invert = Base.negate
+
+instance Infimum Base.Int where
+	(/\) = Base.min
+
+instance Supremum Base.Int where
+	(\/) = Base.max
 
 instance Setoid Base.Int where
 	x == y = if Base.eqInt x y then True else False
