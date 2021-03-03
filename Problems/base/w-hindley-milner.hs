e0c47170ade4b40904c23d18215765abb4797c76
Murat Kasimov <iokasimov.m@gmail.com>
2021-03-03 09:34:21 +0300

    Refactor: renames types and constructors in W algorithm implementation

--- Problems/base/w-hindley-milner.hs
+++ Problems/base/w-hindley-milner.hs
@@ -1,6 +1,6 @@
 module Main where
 
-
+-- Source: https://github.com/wh5a/Algorithm-W-Step-By-Step
 
 import "containers" Data.Map
 import "containers" Data.Set
@@ -9,22 +9,28 @@ import "mtl" Control.Monad.Reader
 import "mtl" Control.Monad.State
 import "pretty" Text.PrettyPrint
 
-data Exp
-	= EVar String
-	| ELit Lit
-	| EApp Exp Exp
-	| EAbs String Exp
-	| ELet String Exp Exp
+data Expression
+	= Variable String
+	| Literal Literal
+	| Application Expression Expression
+	| Abstraction String Expression
+	| Let String Expression Expression
 	deriving (Eq, Ord, Show)
 
-data Lit = LInt Integer | LBool Bool
+data Literal
+	= Integer Integer
+	| Boolean Bool
+	deriving (Eq, Ord, Show)
+
+data Grounded
+	= Integer'
+	| Boolean'
 	deriving (Eq, Ord, Show)
 
 data Monotype
-	= TVar String
-	| TInt
-	| TBool
-	| TFun Monotype Monotype
+	= Grounded Grounded
+	| Parametric String
+	| Function Monotype Monotype
 	deriving (Eq, Ord, Show)
 
 data Polytype = Polytype [String] Monotype
@@ -33,18 +39,18 @@ data Polytype = Polytype [String] Monotype
 type Substitution = Map String Monotype
 
 composeSubst :: Substitution -> Substitution -> Substitution
-composeSubst s1 s2 = Data.Map.union (apply s1 <$> s2)  s1
+composeSubst s1 s2 = Data.Map.union (apply s1 <$> s2) s1
 
 class Types a where
 	ftv :: a -> Set String
 	apply :: Substitution -> a -> a
 
 instance Types Monotype where
-	ftv (TVar n) = Data.Set.singleton n
-	ftv TInt = mempty
-	ftv TBool = mempty
-	ftv (TFun st tt) = Data.Set.union (ftv st) (ftv tt)
-	apply s (TFun st tt) = TFun (apply s st) (apply s tt)
+	ftv (Parametric n) = Data.Set.singleton n
+	ftv (Grounded Integer') = mempty
+	ftv (Grounded Boolean') = mempty
+	ftv (Function st tt) = Data.Set.union (ftv st) (ftv tt)
+	apply s (Function st tt) = Function (apply s st) (apply s tt)
 	apply _ t = t
 
 instance Types Polytype where
@@ -71,7 +77,6 @@ generalize env t = Polytype vars t where
 
 data TIEnv = TIEnv
 
--- data TIState = TIState { tiSupply :: Int }
 type TIState = Int
 
 type TI a = ErrorT String (ReaderT TIEnv (StateT TIState IO)) a
@@ -82,7 +87,7 @@ runTI t = runStateT (runReaderT (runErrorT t) TIEnv) 0
 newTyVar :: String -> TI Monotype
 newTyVar prefix = do
 	modify (+1)
-	TVar . (prefix ++) . show <$> get
+	Parametric . (prefix ++) . show <$> get
 
 instantiate :: Polytype -> TI Monotype
 instantiate (Polytype vars t) = do
@@ -91,44 +96,44 @@ instantiate (Polytype vars t) = do
 	return $ apply s t
 
 mgu :: Monotype -> Monotype -> TI Substitution
-mgu (TFun src tgt) (TFun src' tgt') = do
+mgu (Function src tgt) (Function src' tgt') = do
 	s1 <- mgu src src'
 	s2 <- mgu (apply s1 tgt) (apply s1 tgt')
 	pure $ composeSubst s1 s2
-mgu (TVar u) t = varBind u t
-mgu t (TVar u) = varBind u t
-mgu TInt TInt = pure mempty
-mgu TBool TBool = pure mempty
+mgu (Parametric u) t = varBind u t
+mgu t (Parametric u) = varBind u t
+mgu (Grounded Integer') (Grounded Integer') = pure mempty
+mgu (Grounded Boolean') (Grounded Boolean') = pure mempty
 mgu t t' = throwError $ "Monotypes do not unify: " ++ show t ++ " vs. " ++ show t'
 
 varBind :: String -> Monotype -> TI Substitution
-varBind u t | t == TVar u = pure mempty
+varBind u t | t == Parametric u = pure mempty
 			| u `Data.Set.member` ftv t = throwError $ "occurs check fails: " ++ u ++ " vs. " ++ show t
 			| otherwise = pure $ Data.Map.singleton u t
 
-tiLit :: Lit -> TI (Substitution, Monotype)
-tiLit (LInt _) = return (mempty, TInt)
-tiLit (LBool _) = return (mempty, TBool)
+tiLit :: Literal -> TI (Substitution, Monotype)
+tiLit (Integer _) = return (mempty, Grounded Integer')
+tiLit (Boolean _) = return (mempty, Grounded Boolean')
 
-ti :: Г -> Exp -> TI (Substitution, Monotype)
-ti (Г env) (EVar n) = case Data.Map.lookup n env of
+ti :: Г -> Expression -> TI (Substitution, Monotype)
+ti (Г env) (Variable n) = case Data.Map.lookup n env of
 	Nothing -> throwError $ "Unbound variable: " ++ n
 	Just sigma -> (,) mempty <$> instantiate sigma
-ti _ (ELit l) = tiLit l
-ti env (EAbs n e) = do
+ti _ (Literal l) = tiLit l
+ti env (Abstraction n e) = do
 	tv <- newTyVar "a"
 	let Г env' = remove env n
 	let env'' = Г $ Data.Map.union env' $ Data.Map.singleton n $ Polytype [] tv
 	(s1, t1) <- ti env'' e
-	pure (s1, TFun (apply s1 tv) t1)
-ti env exp@(EApp e1 e2) = do
+	pure (s1, Function (apply s1 tv) t1)
+ti env exp@(Application e1 e2) = do
 	tv <- newTyVar "a"
 	(s1, t1) <- ti env e1
 	(s2, t2) <- ti (apply s1 env) e2
-	s3 <- mgu (apply s2 t1) (TFun t2 tv)
+	s3 <- mgu (apply s2 t1) (Function t2 tv)
 	pure (s3 `composeSubst` s2 `composeSubst` s1, apply s3 tv)
 	`catchError` (\e -> throwError $ e ++ "\n in " ++ show exp)
-ti env (ELet x e1 e2) = do
+ti env (Let x e1 e2) = do
 	(s1, t1) <- ti env e1
 	let Г env' = remove env x
 	let t' = generalize (apply s1 env) t1
@@ -136,15 +141,15 @@ ti env (ELet x e1 e2) = do
 	(s2, t2) <- ti (apply s1 env'') e2
 	pure (s1 `composeSubst` s2, t2)
 
-typeInference :: Map String Polytype -> Exp -> TI Monotype
+typeInference :: Map String Polytype -> Expression -> TI Monotype
 typeInference env e = uncurry apply <$> ti (Г env) e
 
-test :: Exp -> IO ()
+test :: Expression -> IO ()
 test e = fst <$> runTI (typeInference mempty e) >>= \case
 	Left err -> putStrLn $ show e ++ "\n " ++ err ++ "\n"
 	Right t -> putStrLn $ show e ++ " : " ++ show t ++ "\n"
 
 -- let id = \x -> x in id
-e0 = ELet "id" (EAbs "x" (EVar "x")) (EVar "id")
+e0 = Let "id" (Abstraction "x" (Variable "x")) (Variable "id")
 
 main = test e0
