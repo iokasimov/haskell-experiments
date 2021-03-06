5417ed6f2d7e5aec3e4e711ff50dd935fe111331
Murat Kasimov <iokasimov.m@gmail.com>
2021-03-06 14:10:54 +0300

    Refactor a bit

--- Problems/pandora/path-resolver.hs
+++ Problems/pandora/path-resolver.hs
@@ -12,44 +12,30 @@ import "base" Text.Show (Show (show))
 
 import Gears.Instances ()
 
-newtype Directory = Directory (Nonempty Stack Char)
+type Directory = Nonempty Stack Char
 
-instance Show Directory where
-	show (Directory (Construct x (Just xs))) = x : show (Directory xs)
-	show (Directory (Construct x Nothing)) = x : "/"
+type Path = Stack Directory
 
-data Part = Forward Directory | Previous | Current
+data Link = Forward Directory | Previous | Current
 
-instance Show Part where
-	show (Forward dir) = show dir
-	show Previous = "../"
-	show Current = "./"
-
-type Path = Stack Part
-
-instance {-# OVERLAPS #-} Show Path where
-	show (TU (Just (Construct x (Just xs)))) = show x <> show (lift xs :: Path)
-	show (TU (Just (Construct x Nothing))) = show x
-	show (TU Nothing) = ""
-
-settle :: Part -> State (Stack Directory) ()
-settle (Forward dir) = void $ modify @(Stack Directory) (dir +=)
+settle :: Link -> State Path ()
+settle (Forward dir) = void $ modify @Path (dir +=)
 settle Previous = void $ focus @Head @Stack @Directory =<> Nothing
 settle Current = point ()
 
-solution :: Stack Directory
-solution = attached $ run (example ->> settle) empty
+solution :: Stack Link  -> Path
+solution unresolved = attached $ run (unresolved ->> settle) empty
 
 --------------------------------------------------------------------------------
 
 usr, bin, scripts :: Directory
-usr = Directory $ 'u' += 's' += point 'r'
-bin = Directory $ 'b' += 'i' += point 'n'
-scripts = Directory $ 's' += 'c' += 'r' += 'i' += 'p' += 't' += point 's'
+usr = 'u' += 's' += point 'r'
+bin = 'b' += 'i' += point 'n'
+scripts = 's' += 'c' += 'r' += 'i' += 'p' += 't' += point 's'
 
 -- /usr/bin/../bin/./scripts/../
-example :: Stack Part
+example :: Stack Link
 example = Forward usr += Forward bin += Previous += Forward bin
 	+= Current += Forward scripts += Previous += empty
 
-main = void $ Reverse solution ->> print
+main = void $ Reverse (solution example) ->> print
