77046da32d9af67434336e05bc775c81a678fb78
Murat Kasimov <iokasimov.m@gmail.com>
2021-03-27 12:30:09 +0300

    Refactor a bit

--- Problems/pandora/DONE/shaping-pyramid.hs
+++ Problems/pandora/DONE/shaping-pyramid.hs
@@ -1,58 +1,47 @@
-{-# LANGUAGE UndecidableInstances #-}
-
 import "base" Data.Int (Int)
-import "base" Data.List ((++))
 import "base" System.IO (print)
-import "base" Text.Show (show)
 import "pandora" Pandora.Core
 import "pandora" Pandora.Paradigm
 import "pandora" Pandora.Pattern
 import "pandora-io" Pandora.IO
 
 import Gears.Instances ()
-import Gears.Utils (show_zipper_list)
 
-import Debug.Trace (traceShow)
+type Chippable a = (Setoid a, Quasiring a, Group a)
 
-validate :: Zipper List Int -> Conclusion (Zipper List Int) (Zipper List Int)
-validate zipper = resolve @(Zipper List Int) failure (point zipper) . (|- pyramid) . (side <-> side) . run $ lower zipper where
+type Deviation = Zipper List
 
-	pyramid :: Maybe (List Int) -> Maybe (List Int) -> Maybe (Zipper List Int)
-	pyramid ls rs = Tap (extract zipper) <$> (twosome <$> ls <*> rs)
+validate :: forall a . Chippable a => Deviation a
+	-> Conclusion (Deviation a) (Deviation a)
+validate z = resolve @(Deviation a) failure (point z)
+	. (|- pyramid) . (side <-> side) . run $ lower z where
 
-	side :: List Int -> Maybe (List Int)
-	side xs = extract <$> run (xs ->> slide) (extract zipper)
+	pyramid :: Maybe (List a) -> Maybe (List a) -> Maybe (Deviation a)
+	pyramid ls rs = Tap (extract z) <$> (twosome <$> ls <*> rs)
 
-	slide :: Int -> State Int :> Maybe := Int
-	slide now = current @Int >>= \before ->
-		before - now == 1 ? replace @Int now $ nothing
+	side :: List a -> Maybe (List a)
+	side xs = extract <$> run (xs ->> slide) (extract z)
 
-type Enumeration = Comprehension Maybe
+	slide :: forall a . Chippable a => a -> State a :> Maybe := a
+	slide now = current @a >>= \before ->
+		before - now == one ? replace @a now $ nothing
 
-explore :: Zipper List Int -> Conclusion (Zipper List Int) :. Zipper List :. Zipper List := Int
+explore :: forall a . Chippable a => Deviation a
+	-> Conclusion (Deviation a) :. Deviation :. Deviation := a
 explore bricks = chipped <$> sequence (bricks =>> validate) where
 
-	chipped :: Zipper List (Zipper List Int) -> Zipper List (Zipper List Int)
+	chipped :: Deviation (Deviation a) -> Deviation (Deviation a)
 	chipped shifted = over (focus @Head) decrement <$> shifted
 
-	decrement :: (Setoid a, Quasiring a, Group a) => a -> a
+	decrement :: a -> a
 	decrement x = x == zero ? zero $ x - one
 
-nonempty_list_to_zipper_list :: Nonempty List ~> Zipper List
-nonempty_list_to_zipper_list xs = Tap (extract xs) $ twosome / unite (deconstruct xs) / empty
-
-zipper_list_to_list :: Zipper List ~> List
-zipper_list_to_list (Tap x (T_U (bs :*: fs))) =
-	attached . run @(State _) % item @Push x bs
-		$ fs ->> modify @(List _) . item @Push
-
-wrong_example, valid_example :: Nonempty List Int
-wrong_example = item @Push 0 $ item @Push 1 $ item @Push 5
+not_valid_example, valid_example :: Nonempty List Int
+not_valid_example = item @Push 0 $ item @Push 1 $ item @Push 4
 	$ item @Push 3 $ item @Push 2 $ item @Push 1 $ point 0
 valid_example = item @Push 0 $ item @Push 1 $ item @Push 2
 	$ item @Push 3 $ item @Push 2 $ item @Push 1 $ point 0
 
 main = void $ do
-	print "..................."
-	let wrong = nonempty_list_to_zipper_list wrong_example
-	print $ (explore wrong >>= (->> explore) >>= (->>> explore) >>= (->>>> explore))
+	let not_valid = into @(Zipper List) not_valid_example
+	print $ explore not_valid >>= (->> explore) >>= (->>> explore)
