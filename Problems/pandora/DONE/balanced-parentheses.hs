aa7ee64425c7ba2b3dd7f27ce44181e6b048d92e
Murat Kasimov <iokasimov.m@gmail.com>
2021-03-28 12:09:14 +0300

    Use Sign type from ASCII module in balanced parentheses problem

--- Problems/pandora/DONE/balanced-parentheses.hs
+++ Problems/pandora/DONE/balanced-parentheses.hs
@@ -1,31 +1,26 @@
 import "pandora" Pandora.Core
 import "pandora" Pandora.Paradigm
 import "pandora" Pandora.Pattern
+import "pandora-io" Pandora.IO
 
 import Prelude (IO, Int, Show (show), print)
 import Gears.Instances ()
 
-data Shape = Opened | Closed
-
-data Style = Round | Square | Angle | Curly
-
-instance Setoid Style where
+instance Setoid Bracket where
 	Round == Round = True
 	Square == Square = True
 	Angle == Angle = True
 	Curly == Curly = True
 	_ == _ = False
 
-data Symbol = Bracket Style Shape | Nevermind
-
 type Index = Int
 
-type Open = Style :*: Index
+type Open = Bracket :*: Index
 
 data Stumble
-	= Deadend Style Index -- Closed bracket without opened one
-	| Logjam Style Index -- Opened bracket without closed one
-	| Mismatch Style Index Style Index -- Closed bracket doesn't match opened one
+	= Deadend Bracket Index -- Closed bracket without opened one
+	| Logjam Bracket Index -- Opened bracket without closed one
+	| Mismatch Bracket Index Bracket Index -- Closed bracket doesn't match opened one
 
 type Opened = List Open
 
@@ -33,64 +28,66 @@ type Trace = Index :*: Opened
 
 type Checking t = (Applicative t, Monad t, Stateful Trace t, Failable Stumble t)
 
-inspect :: Checking t => Symbol -> t ()
+inspect :: Checking t => Sign -> t ()
 inspect s = indexate *> decide s
 
 indexate :: Checking t => t Index
 indexate = adjust @Trace @Index (+ one)
 
-decide :: Checking t => Symbol -> t ()
-decide (Bracket opened Opened) = remember opened
-decide (Bracket closed Closed) = latest (deadend closed) (juxtapose closed)
-decide Nevermind = skip
+decide :: Checking t => Sign -> t ()
+decide (Bracket Opened opened) = remember opened
+decide (Bracket Closed closed) = latest (deadend closed) (juxtapose closed)
+decide _ = skip
 
-remember :: Checking t => Style -> t ()
+remember :: Checking t => Bracket -> t ()
 remember style = void . adjust @Trace @Opened . (!) =<< item @Push
 	<$> ((:*:) style <$> magnify @Trace @Index)
 	<*> magnify @Trace @Opened
 
 -- TODO: it looks too complicated
-latest :: Checking t => t r -> (Index -> Style -> t r) -> t ()
+latest :: Checking t => t r -> (Index -> Bracket -> t r) -> t ()
 latest on_empty f = void $ zoom @Trace (focus @Right |> focus @Head) current
 	>>= resolve @Open @(Maybe Open) (|- f) on_empty
 
-juxtapose :: Checking t => Style -> Index -> Style -> t ()
+juxtapose :: Checking t => Bracket -> Index -> Bracket -> t ()
 juxtapose closed oi opened = closed != opened ? mismatch closed opened oi
 	$ void . adjust @Trace @Opened $ view / sub @Tail
 
 skip :: Pointable t => t ()
 skip = point ()
 
-mismatch :: Checking t => Style -> Style -> Index -> t ()
+mismatch :: Checking t => Bracket -> Bracket -> Index -> t ()
 mismatch closed opened oi = magnify @Trace @Index
 	>>= failure . Mismatch opened oi closed
 
-logjam :: Checking t => Index -> Style -> t ()
+logjam :: Checking t => Index -> Bracket -> t ()
 logjam i = failure . Logjam % i
 
-deadend :: Checking t => Style -> t ()
+deadend :: Checking t => Bracket -> t ()
 deadend closed = magnify @Trace @Index >>= failure . Deadend closed
 
 --------------------------------------------------------------------------------
 
 type Checker = State (Index :*: Opened) :> Conclusion Stumble
 
-check :: Traversable s => s Symbol -> Checker ()
+check :: Traversable s => s Sign -> Checker ()
 check code = code ->> inspect *> latest skip logjam
 
 interpret :: Checker () -> IO ()
 interpret result = result & run % (1 :*: empty) & conclusion print ((print "OK") !)
 
-deriving instance Show Shape
-deriving instance Show Style
-deriving instance Show Symbol
+deriving instance Show Bracket
+deriving instance Show Sign
 deriving instance Show Stumble
+deriving instance Show Quote
+deriving instance Show Slash
+deriving instance Show Position
 
-example_ok, example_mismatch, example_deadend, example_logjam :: List Symbol
-example_ok = item @Push (Bracket Curly Opened) $ item @Push Nevermind $ item @Push (Bracket Curly Closed) $ empty  -- {x}
-example_mismatch = item @Push (Bracket Curly Opened) $ item @Push (Bracket Square Closed) $ empty -- {]
-example_deadend = item @Push (Bracket Round Closed) $ empty -- )
-example_logjam = item @Push (Bracket Angle Opened) $ empty -- <
+example_ok, example_mismatch, example_deadend, example_logjam :: List Sign
+example_ok = item @Push (Bracket Opened Curly) $ item @Push Underscore $ item @Push (Bracket Closed Curly) $ empty  -- {_}
+example_mismatch = item @Push (Bracket Opened Curly) $ item @Push (Bracket Closed Square) $ empty -- {]
+example_deadend = item @Push (Bracket Closed Round) $ empty -- )
+example_logjam = item @Push (Bracket Opened Angle) $ empty -- <
 
 main = do
 	interpret $ check example_ok
