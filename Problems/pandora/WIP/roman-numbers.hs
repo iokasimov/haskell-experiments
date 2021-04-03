0ef9f5a930163a5a31c8766b04908e668991d3d6
Murat Kasimov <iokasimov.m@gmail.com>
2021-04-03 13:51:23 +0300

    [WIP] Typed tubles indexed by roman literals

--- Problems/pandora/WIP/roman-numbers.hs
+++ Problems/pandora/WIP/roman-numbers.hs
@@ -3,39 +3,75 @@ module Main where
 import "pandora" Pandora.Core
 import "pandora" Pandora.Paradigm
 import "pandora" Pandora.Pattern
+import "pandora-io" Pandora.IO
 
 import Prelude (Int, print)
 
-data Symbol = I | V | X | L | C | D | M
+-- data Symbol = I | V | X | L | C | D | M
+--
+-- type Place (d :: Symbol) = Tagged d ()
+--
+-- data Number (o :: Symbol) (f :: Symbol) (t :: Symbol)
+-- 	= D1 (Place o)
+-- 	| D2 (Place o) (Place o)
+-- 	| D3 (Place o) (Place o) (Place o)
+-- 	| D4 (Place o) (Place f)
+-- 	| D5 (Place f)
+-- 	| D6 (Place f) (Place o)
+-- 	| D7 (Place f) (Place o) (Place o)
+-- 	| D8 (Place f) (Place o) (Place o) (Place o)
+-- 	| D9 (Place o) (Place t)
+--
+-- data family Decimal (o :: Symbol) (f :: Symbol) (t :: Symbol)
+-- data instance Decimal I V X = Unit
+-- data instance Decimal X L C = Ten
+-- data instance Decimal C D M = Hundred
+--
+-- class Numberify a where
+-- 	numberify :: a -> Int
+--
+-- instance Numberify Symbol where
+-- 	numberify I = 1
+-- 	numberify V = 5
+-- 	numberify X = 10
+-- 	numberify L = 50
+-- 	numberify C = 100
+-- 	numberify D = 500
+-- 	numberify M = 1000
 
-type Place (d :: Symbol) = Tagged d ()
+-- type family (.) a b
 
-data Number (o :: Symbol) (f :: Symbol) (t :: Symbol)
-	= D1 (Place o)
-	| D2 (Place o) (Place o)
-	| D3 (Place o) (Place o) (Place o)
-	| D4 (Place o) (Place f)
-	| D5 (Place f)
-	| D6 (Place f) (Place o)
-	| D7 (Place f) (Place o) (Place o)
-	| D8 (Place f) (Place o) (Place o) (Place o)
-	| D9 (Place o) (Place t)
+-- X .. V .. I .
 
-data family Decimal (o :: Symbol) (f :: Symbol) (t :: Symbol)
-data instance Decimal I V X = Unit
-data instance Decimal X L C = Ten
-data instance Decimal C D M = Hundred
+infixr 1 #=
 
-class Numberify a where
-	numberify :: a -> Int
+type family (#=) (l :: Letter) b where
+	I #= a = a
+	V #= a = a :*: a :*: a :*: a :*: a
+	X #= a = a :*: a :*: a :*: a :*: a :*: a :*: a :*: a :*: a :*: a
 
-instance Numberify Symbol where
-	numberify I = 1
-	numberify V = 5
-	numberify X = 10
-	numberify L = 50
-	numberify C = 100
-	numberify D = 500
-	numberify M = 1000
+type family (=#) (l :: Letter) b where
+	I =# (a :*: r) = r
+	V =# (a :*: a :*: a :*: a :*: a :*: r) = r
+	X =# (a :*: a :*: a :*: a :*: a :*: a :*: a :*: a :*: a :*: a :*: r) = r
 
-main = print "typechecked"
+-- v5_to_list :: forall t a . Monotonic a (t a) => t a -> List a
+-- v5_to_list = reduce @a @(t a) (item @Push @List) empty
+
+-- example :: V (V Int)
+-- example =
+-- 	(0 :*: 0 :*: 0 :*: 0 :*: 0) :*:
+-- 	(0 :*: 0 :*: 0 :*: 0 :*: 0) :*:
+-- 	(0 :*: 0 :*: 0 :*: 0 :*: 0) :*:
+-- 	(0 :*: 0 :*: 0 :*: 0 :*: 0) :*:
+-- 	(0 :*: 0 :*: 0 :*: 0 :*: 0)
+
+vi :: V #= (I #= Int)
+vi = 0 :*: 0 :*: 0 :*: 0 :*: 0
+
+iv :: I =# (V #= Int)
+iv = 0 :*: 0 :*: 0 :*: 0
+
+main = void $ do
+	print "typechecked"
+	-- print . v5_to_list $ (True :*: True :*: True :*: False :*: False :: V Boolean) 
