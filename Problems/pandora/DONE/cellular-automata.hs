7eabc45ad922492ce83eb81dda79e009d6c0f202
Murat Kasimov <iokasimov.m@gmail.com>
2021-05-31 07:08:42 +0300

    Fix cellular automata problem solution

--- Problems/pandora/DONE/cellular-automata.hs
+++ Problems/pandora/DONE/cellular-automata.hs
@@ -15,11 +15,11 @@ import Prelude (IO, Char, Int, putStr, print, reverse, (-), (<>))
 
 type Status = Boolean
 
-type Field = Zipper Stream Status
+type Field = Zipper Stream (Left ::: Right) Status
 
 type Neighbours = Status :*: Status :*: Status
 
-start :: Zipper Stream Status
+start :: Zipper Stream (Left ::: Right) Status
 start = let desert = repeat False in Tap True $ twosome desert desert
 
 rule90 :: Neighbours -> Status
@@ -32,17 +32,14 @@ rule90 (False :*: True :*: False) = False
 rule90 (False :*: False :*: True) = True
 rule90 (False :*: False :*: False) = False
 
-neighbourhood :: Zipper Stream Status -> Neighbours
-neighbourhood z = extract (view # focus @Left $ run # lower z)
-	:*: extract z :*: extract (view # focus @Right $ run # lower z)
+neighbourhood :: Zipper Stream (Left ::: Right) Status -> Neighbours
+neighbourhood z = extract (extract $ view # sub @Left # lower z)
+	:*: extract z :*: extract (extract $ view # sub @Right # lower z)
 
-display :: Int -> Zipper Stream a -> [a]
+display :: Int -> Zipper Stream (Left ::: Right) a -> [a]
 display n (Tap x (T_U (bs :*: fs))) = reverse (take n $ stream_to_list bs) <> [x] <> take n (stream_to_list fs)
 
-instance Monotonic a (Construction Identity a) where
-	reduce f r ~(Construct x (Identity xs)) = f x $ reduce f r xs
-
-record :: (Zipper Stream Status -> Status) -> Zipper Stream Status -> IO ()
+record :: (Zipper Stream (Left ::: Right) Status -> Status) -> Zipper Stream (Left ::: Right) Status -> IO ()
 record act being = delay *> snapshot *> evolve where
 
 	evolve, snapshot :: IO ()
@@ -59,13 +56,15 @@ type II = Tap Sides <:.> Tap Sides
 
 type Sides = Stream <:.:> Stream := (:*:)
 
-type Horizontally = Zipper Stream <:.> Stream
-type Vertically = Stream <:.> Zipper Stream
+type Horizontally = Zipper Stream (Left ::: Right) <:.> Stream
+
+-- TODO: There we should use Up :: Down instead of Left ::: Right, but let's change it later
+type Vertically = Stream <:.> Zipper Stream (Left ::: Right)
 
 instance Extendable II where
 	zz =>> f = f <$> TU (horizontal <$> vertical zz) where
 
-		horizontal, vertical :: II a -> Zipper Stream (II a)
+		horizontal, vertical :: II a -> Zipper Stream (Left ::: Right) (II a)
 		horizontal z = Tap z $ twosome # move ((rotate @Left <$>) ||=) z # move ((rotate @Right <$>) ||=) z
 		vertical z = Tap z $ twosome # move (rotate @Left ||=) z # move (rotate @Right ||=) z
 
@@ -73,38 +72,46 @@ instance Extendable II where
 		move f x = extract . deconstruct $ point . f .-+ x
 
 instance Substructure Down II where
-	type Substructural Down II = Vertically
-	substructure = PQ_ $ \ii -> case run . extract . run # ii of
-		Tap focused (T_U (d :*: u)) -> Store $ TU d :*: lift . TU . Tap focused . (twosome % u) . run
+	type Available Down II = Identity
+	type Substance Down II = Vertically
+	substructure = P_Q_T $ \ii -> case run . extract . run # ii of
+		Tap focused (T_U (d :*: u)) -> Store $ Identity (TU d) :*: lift . TU . Tap focused . (twosome % u) . run . extract
 
 instance Substructure Up II where
-	type Substructural Up II = Vertically
-	substructure = PQ_ $ \x -> case run . extract . run # x of
-		Tap focused (T_U (d :*: u)) -> Store $ TU u :*: lift . TU . Tap focused . twosome d . run
+	type Available Up II = Identity
+	type Substance Up II = Vertically
+	substructure = P_Q_T $ \x -> case run . extract . run # x of
+		Tap focused (T_U (d :*: u)) -> Store $ Identity (TU u) :*: lift . TU . Tap focused . twosome d . run . extract
 
 instance Covariant t => Substructure Left (t <:.:> t := (:*:)) where
-	type Substructural Left (t <:.:> t := (:*:)) = t
-	substructure = PQ_ (Store . (lift .#.. (twosome %) <$>) . run . extract . run)
+	type Available Left (t <:.:> t := (:*:)) = Identity
+	type Substance Left (t <:.:> t := (:*:)) = t
+	substructure = P_Q_T $ \x -> case run # lower x of
+		ls :*: rs -> Store $ Identity ls :*: lift . (twosome % rs) . extract
 
 instance Covariant t => Substructure Right (t <:.:> t := (:*:)) where
-	type Substructural Right (t <:.:> t := (:*:)) = t
-	substructure = PQ_ (Store . (lift .#.. twosome <$>) . swap . run . extract . run)
+	type Available Right (t <:.:> t := (:*:)) = Identity
+	type Substance Right (t <:.:> t := (:*:)) = t
+	substructure = P_Q_T $ \x -> case run # lower x of
+		ls :*: rs -> Store $ Identity rs :*: lift . (twosome ls) . extract
 
 instance Substructure Left II where
-	type Substructural Left II = Horizontally
-	substructure = PQ_ $ \x -> case run . extract . run # x of
-		Tap x (T_U (d :*: u)) -> let left = sub @Left . sub @Tail in
-			let target = TU $ Tap # view left x $ twosome # view left <$> d # view left <$> u in
-			let around lx = twosome # set left <$> view left lx <*> d # set left <$> view left lx <*> u in
-			Store $ target :*: \lx -> lift . TU . Tap (set left # extract (run lx) # x) $ around # run lx
+	type Available Left II = Identity
+	type Substance Left II = Horizontally
+	substructure = P_Q_T $ \x -> case run # lower x of
+		Tap focused (T_U (ds :*: us)) ->
+			let target = TU $ Tap # (extract $ view (sub @Left) focused) $ twosome # extract . view (sub @Left) <$> ds # extract . view (sub @Left) <$> us in
+			let around lx = twosome # (set (sub @Left) . Identity <$> (extract $ view (sub @Left) lx) <*> ds) # (set (sub @Left) . Identity <$> (extract $ view (sub @Left) lx) <*> us) in
+			Store $ Identity target :*: \lx -> lift . TU . Tap (set (sub @Left) # Identity (extract (run (extract lx))) # focused) $ around # run (extract lx)
 
 instance Substructure Right II where
-	type Substructural Right II = Horizontally
-	substructure = PQ_ $ \x -> case run . extract . run # x of
-		Tap x (T_U (d :*: u)) -> let right = sub @Right . sub @Tail in
-			let target = TU . Tap (view right x) $ twosome # view right <$> d # view right <$> u in
-			let around rx = twosome # set right <$> view right rx <*> d # set right <$> view right rx <*> u in
-			Store $ target :*: \rx -> lift . TU . Tap (set right # extract (run rx) # x) $ around # run rx
+	type Available Right II = Identity
+	type Substance Right II = Horizontally
+	substructure = P_Q_T $ \x -> case run # lower x of
+		Tap focused (T_U (ds :*: us)) ->
+			let target = TU . Tap (extract $ view (sub @Right) focused) $ twosome # extract . view (sub @Right) <$> ds # extract . view (sub @Right) <$> us in
+			let around rx = twosome # set (sub @Right) . Identity <$> (extract $ view (sub @Right) rx) <*> ds # set (sub @Right) . Identity <$> (extract $ view (sub @Right) rx) <*> us in
+			Store $ Identity target :*: \rx -> lift . TU . Tap (set (sub @Right) # Identity (extract (run (extract rx))) # focused) $ around # run (extract rx)
 
 type Around = Status -- current
 	:*: Status :*: Status -- horizontal
@@ -116,11 +123,11 @@ around :: II Status -> Around
 around z = extract z :*: plane @Left :*: plane @Right :*: plane @Up :*: plane @Down
 	:*: slant @Down @Tail @Left :*: slant @Up @Tail @Right :*: slant @Up @Tail @Left :*: slant @Down @Tail @Right where
 
-	plane :: forall i t u . (Substructured i II (t <:.> u), Extractable t, Extractable u) => Status
-	plane = extract . extract . run $ view # sub @i # z
+	plane :: forall i t u . (Substructured i II Identity (t <:.> u), Extractable t, Extractable u) => Status
+	plane = extract . lower . extract $ view # sub @i # z
 
-	slant :: forall v q h . (Substructured v II Vertically, Substructured q (Zipper Stream) Sides, Substructured h Sides Stream) => Status
-	slant = extract . view (sub @h . sub @q) . extract . run $ view # sub @v # z
+	slant :: forall v q h . (Substructured v II Identity Vertically, Substructured q (Zipper Stream (Left ::: Right)) Identity Sides, Substructured h Sides Identity Stream) => Status
+	slant = extract . extract . view (sub @h . sub @q) . lower . extract $ view # sub @v # z
 
 conway :: Around -> Status
 conway (focused :*: neighbors) = alive == one + one ? focused
@@ -143,10 +150,10 @@ lifecycle act being = delay *> purge *> snapshot *> evolve where
 cube :: II Status
 cube = TU . Tap one $ twosome only only where
 
-	only :: Stream :. Zipper Stream := Status
+	only :: Stream :. Zipper Stream (Left ::: Right) := Status
 	only = Construct one . Identity $ repeat noone
 
-	one, noone :: Zipper Stream Status
+	one, noone :: Zipper Stream (Left ::: Right) Status
 	one = Tap True $ twosome alone alone
 	noone = Tap False $ twosome # repeat False # repeat False
 
@@ -156,7 +163,7 @@ cube = TU . Tap one $ twosome only only where
 blinker :: II Status
 blinker = TU . Tap one $ twosome # repeat noone # repeat noone where
 
-	one, noone :: Zipper Stream Status
+	one, noone :: Zipper Stream (Left ::: Right) Status
 	one = Tap True $ twosome alone alone
 	noone = Tap False $ twosome # repeat False # repeat False
 
