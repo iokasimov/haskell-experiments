f6d848a7f78f7b4fb7c99d474dc82ec19765d9bc
Murat Kasimov <iokasimov.m@gmail.com>
2022-01-16 15:30:16 +0300

    Start coding typing trainer

--- Problems/pandora/WIP/typing-trainer.hs
+++ Problems/pandora/WIP/typing-trainer.hs
@@ -10,26 +10,4 @@ import "base" Text.Show (Show (show))
 
 import Gears.Instances ()
 
-display (Turnover z) = void ! do
-	print . (" L " <>) . show <<-<<- view (sub @Left) z
-	print . (" * " <>) . show <<-<<- view (sub @Root) z
-	print . (" R " <>) . show <<-<<- view (sub @Right) z
-
-example :: Tape List Int
-example = twosome (Identity 3) ! twosome
-	# Reverse (lift . Construct 2 . Just # Construct 1 Nothing)
-	# lift (Construct 4 . Just # Construct 5 Nothing)
-
-main = void ! do
-	print "---------"
-	display ! Turnover example
-	print "---------"
-	print "---------"
-	display ! rotate @Right # Turnover example
-	print "---------"
-	print "---------"
-	display ! rotate @Right . rotate @Right # Turnover example
-	print "---------"
-	print "---------"
-	display ! rotate @Right . rotate @Right . rotate @Right # Turnover example
-	print "---------"
+main = print "typechecked"
